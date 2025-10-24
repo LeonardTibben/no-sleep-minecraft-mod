c9e71d5c0ff145123235fb9a278ae63966f98192
Leonard Tibben <leonard@baseflow.com>
2025-10-24 21:02:28 +0200

    template

---
+++ src/client/java/lenny/nosleeping/NoSleepingClient.java
@@ -0,0 +1,10 @@
+package lenny.nosleeping;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoSleepingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
