10d2f0a730e21b453645c959583760377a97d98d
leonard <leonard@baseflow.com>
2025-12-28 19:27:34 +0100

    fixed respawn

--- src/main/java/lenny/nosleeping/mixin/BedMixin.java
+++ src/main/java/lenny/nosleeping/mixin/BedMixin.java
@@ -3,6 +3,8 @@ package lenny.nosleeping.mixin;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.enums.BedPart;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.ServerAdvancementLoader;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -11,7 +13,13 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProperties;
+import net.minecraft.world.attribute.BedRule;
+import net.minecraft.world.attribute.EnvironmentAttributes;
+import net.minecraft.world.explosion.ExplosionBehavior;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,28 +27,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
+import static net.minecraft.block.BedBlock.PART;
+import static net.minecraft.block.HorizontalFacingBlock.FACING;
+
 @Mixin(BedBlock.class)
 public class BedMixin {
 
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
     protected void injectOnUseMethod(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         if (player instanceof ServerPlayerEntity) {
+
             ServerAdvancementLoader serverAdvancementLoader = player.getEntityWorld().getServer().getAdvancementLoader();
 
             var players = world.getPlayers();
 
             AdvancementEntry killDragon = serverAdvancementLoader.get(Identifier.tryParse("minecraft:end/kill_dragon"));
-            AdvancementEntry killWither = serverAdvancementLoader.get(Identifier.tryParse("minecraft:nether/create_beacon"));
-            AdvancementEntry defeatRaid = serverAdvancementLoader.get(Identifier.tryParse("minecraft:adventure/hero_of_the_village"));
 
             for (int i = 0; i < players.size(); i++) {
 
                 if (!((ServerPlayerEntity) player).getAdvancementTracker().getProgress(killDragon).isDone()
-                        && !((ServerPlayerEntity) player).getAdvancementTracker().getProgress(killWither).isDone()
-                        && !((ServerPlayerEntity) player).getAdvancementTracker().getProgress(defeatRaid).isDone()) {
+                ) {
+                    ((ServerPlayerEntity) player).setSpawnPointFrom((ServerPlayerEntity) player);
                     Text literal = Text.of("You may not rest yet, there is still work to be done!");
                     player.sendMessage(literal, false);
                     cir.setReturnValue(ActionResult.FAIL);
+
+                    ((ServerPlayerEntity) player).setSpawnPoint(new ServerPlayerEntity.Respawn(WorldProperties.SpawnPoint.create(player.getEntityWorld().getRegistryKey(), pos, player.getYaw(), player.getPitch()), false), false);
                     cir.cancel();
                     i = players.size();
                 }
